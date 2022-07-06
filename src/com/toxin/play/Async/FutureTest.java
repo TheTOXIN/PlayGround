a2d746af2be506fb331797538b31107307d8bf1b
Y.Belousov <Y.Belousov@russianpost.ru>
2022-07-06 22:47:57 +0300

    пошта

--- src/com/toxin/play/Async/FutureTest.java
+++ src/com/toxin/play/Async/FutureTest.java
@@ -1,10 +1,18 @@
 package com.toxin.play.Async;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class FutureTest {
 
     public static void main(String[] args) throws Exception {
+        test_1();
+        test_2();
+    }
+
+    private static void test_1() throws Exception {
         Callable<String> task = () -> {
             try {
                 TimeUnit.SECONDS.sleep(1);
@@ -24,11 +32,19 @@ public class FutureTest {
         System.out.println(future.isDone());
         System.out.println(future.get());
 
-        CompletableFuture completableFuture = CompletableFuture
-            .completedFuture("я из будущего")
-            .exceptionally(Throwable::getMessage)
-            .thenAccept(System.out::println);
+        CompletableFuture<Void> completableFuture = CompletableFuture
+                .completedFuture("я из будущего")
+                .exceptionally(Throwable::getMessage)
+                .thenAccept(System.out::println);
 
         completableFuture.join();
+    }
+
+    private static void test_2() throws Exception {
+//        List<Future<String>> tasks = new ArrayList<>();
+//
+//        Stream.of("TASK_1", "TASK_2", "TASK_3").forEach(t -> {
+//            tasks.add()
+//        });
     }
 }
